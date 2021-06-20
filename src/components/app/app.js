c7d1b286347213ecd2a4a46df558593fa22f4f5d
Mariya Frantsava <53209068+mariyafrantsava@users.noreply.github.com>
2021-06-20 23:05:59 +0300

    init: start pr notes

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import './app.scss';
+
+export default class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>hi</p>
+            </div>
+        );
+    }
+}
